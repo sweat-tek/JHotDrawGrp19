a01815dcb23d88c9dac073f3afcc59d3b4f21674
Muhamad Nadali <munad20@student.sdu.dk>
2023-01-02 16:46:06 +0100

    arrangeTestPackage

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/arrangetest/SendToBackActionTest.java
@@ -0,0 +1,54 @@
+package arrangetest;
+
+import org.jhotdraw.draw.DefaultDrawing;
+import org.jhotdraw.draw.DefaultDrawingView;
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.action.SendToBackAction;
+import org.jhotdraw.draw.figure.Figure;
+import org.jhotdraw.draw.figure.RectangleFigure;
+import org.junit.*;
+
+
+public class SendToBackActionTest {
+
+
+    public SendToBackActionTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void sendtobackTest() throws InterruptedException {
+        Drawing drawing = new DefaultDrawing();
+        Figure figure1 = new RectangleFigure(0, 0, 10, 10);
+        Figure figure2 = new RectangleFigure(10, 10, 10, 10);
+        drawing.add(figure1);
+        drawing.add(figure2);
+        // create a drawing view with the drawing
+        DrawingView view = new DefaultDrawingView();
+        view.setDrawing(drawing);
+
+        // select both figures
+        view.addToSelection(figure1);
+        view.addToSelection(figure2);
+
+        SendToBackAction.sendToBack(view, view.getSelectedFigures());
+
+    }
+
+}
